2a8de34feef8b6d4a5d60ee353f983167691db40
CB17001 <muhd.umar699@gmail.com>
2021-03-11 14:45:02 +0800

    End of session 8

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,105 +1,34 @@
-import React, { useEffect } from 'react';
 import { Container, } from 'semantic-ui-react';
-//import { Activity } from '../models/activity';
 import NavBar from './NavBar';
-import { Fragment } from 'react';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-//import {v4 as uuid} from 'uuid'
-//import agent from '../api/agent';
-import LoadingComponent from './LoadingComponent';
-import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
+import { Route, useLocation } from 'react-router';
+import HomePage from '../../features/home/HomePage';
+import ActivityForm from '../../features/activities/form/ActivityForm';
+import ActivityDetails from '../../features/activities/details/ActivityDetails';
 
 function App() {
 
-  const {activityStore} = useStore()
-
-  /*const [activities, setActivities] = useState<Activity[]>([]);
-  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
-  const [editMode, setEditMode] = useState(false)
-  const [submitting, setSubmitting] = useState(false);*/
-
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore]);
-
-/*
-  function handleSelectActivity(id: string) {
-    setSelectedActivity(activities.find(x => x.id === id));
-  }
-
-  function handleCancelSelectActivity() {
-    setSelectedActivity(undefined)
-  }
-
-  function handleFormOpen(id?: string) {
-    id ? handleSelectActivity(id) : handleCancelSelectActivity();
-    setEditMode(true);
-  }
-
-  function handleFormClose() {
-    setEditMode(false);
-  }
-
-  function handleCreateOrEditActivity(activity: Activity) {
-    setSubmitting(true);
-    if (activity.id) {
-      agent.Activities.update(activity).then(() => {
-        setActivities([...activities.filter(x => x.id !== activity.id), activity])
-        setSelectedActivity(activity);
-        setEditMode(false);
-        setSubmitting(false);
-      })
-    } else {
-      activity.id = uuid();
-      agent.Activities.create(activity).then(() => {
-        setActivities([...activities, activity])
-        setSelectedActivity(activity);
-        setEditMode(false);
-        setSubmitting(false);
-      })
-    }
-  }
-
-  function handleDeleteActivity(id: string) {
-    setSubmitting(true);
-    agent.Activities.delete(id).then(() => {
-      setActivities([...activities.filter(x => x.id !== id)]);
-      setSubmitting(false);
-    })
-    
-  }
-*/
-  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
+  const location = useLocation();
 
   return (
-    <Fragment>
-      <NavBar />
-      <Container style={{marginTop: '7em'}}>
-        <ActivityDashboard />
-      </Container>
-    </Fragment>
+    <>
+      <Route exact path='/' component={HomePage} />
+      <Route
+        path={'/(.+)'}
+        render={() => (
+          <>
+            <NavBar />
+            <Container style={{marginTop: '7em'}}>
+              <Route exact path='/activities' component={ActivityDashboard} />
+              <Route path='/activities/:id' component={ActivityDetails} />
+              <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
+            </Container>
+          </>
+        )}
+      />
+    </>
   );
 }
 
 export default observer(App);
-
-/*return (
-  <Fragment>
-    <NavBar openForm={handleFormOpen} />
-    <Container style={{marginTop: '7em'}}>
-      <ActivityDashboard
-        activities={activityStore.activities} 
-        selectedActivity={selectedActivity} 
-        selectActivity={handleSelectActivity}  
-        cancelSelectActivity={handleCancelSelectActivity} 
-        editMode={editMode} 
-        openForm={handleFormOpen} 
-        closeForm={handleFormClose} 
-        createOrEdit={handleCreateOrEditActivity} 
-        deleteActivity={handleDeleteActivity} 
-        submitting={submitting} 
-      />
-    </Container>
-  </Fragment>
-);*/
